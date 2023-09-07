5944a537e36f3aad682c3ac8a76c318c757d5d92
ommeleven <dangeom94@gmail.com>
2023-09-06 22:02:53 -0500

    set up price calc method

--- project2.py
+++ project2.py
@@ -1,8 +1,16 @@
 class Item:
-    pass
+    def calculate_total_price(self, x, y):
+        return x * y
+
 item1 = Item()
 item1.price = 100
 item1.name = 'Phone'
 item1.quantity = 5
+print(item1.calculate_total_price(item1.price, item1.quantity))
 
-print(item1.quantity)
+   
+item2 = Item()
+item2.price = 1000
+item2.name = 'Laptop'
+item2.quantity = 4
+print(item2.calculate_total_price(item2.price, item2.quantity))
